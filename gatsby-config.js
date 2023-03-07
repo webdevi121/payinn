1ccc6d9b76e68849ce5012d7444cba1bd6bdf675
Sarniel Morgia <sarniel@Sarniels-MBP.gateway>
2023-03-07 21:57:52 +1100

    revert back packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sitemap`,
-    `gatsby-theme-headless-wordpress`,
     {
       resolve: "gatsby-plugin-sass",
       options: {
